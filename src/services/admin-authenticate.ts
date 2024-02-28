08da4b3e491872601e95ebd32ffd762f03d3eaf5
ricsjs <ricfilho0007@gmail.com>
2024-02-28 20:29:43 -0300

    feat: admin authenticate service

---
+++ src/services/admin-authenticate.ts
@@ -0,0 +1,37 @@
+import { Admin } from "@prisma/client";
+import { AdminsRepository } from "../repositories/admins-repository";
+import { InvalidCredentialError } from "./errors/invalid-credential-error";
+import { compare } from "bcryptjs";
+
+interface AdminAuthenticateServiceRequest {
+    email: string
+    password: string
+}
+
+interface AdminAuthenticateServiceResponse {
+    admin: Admin
+}
+
+export class AdminAuthenticate {
+    constructor(
+        private adminsRepository: AdminsRepository
+    ) {}
+
+    async execute({ email, password }: AdminAuthenticateServiceRequest): Promise<AdminAuthenticateServiceResponse> {
+        const admin = await this.adminsRepository.findByEmail(email)
+
+        if (!admin) {
+            throw new InvalidCredentialError()
+        }
+
+        const doesPasswordMatches = await compare(password, admin.password_hash)
+
+        if (!doesPasswordMatches) {
+            throw new InvalidCredentialError()
+        }
+
+        return {
+            admin
+        }
+    }
+}
