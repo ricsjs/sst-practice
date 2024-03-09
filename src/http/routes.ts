391829df7f83c91523950da2dd885543cba2b428
ricsjs <ricfilho0007@gmail.com>
2024-03-09 14:47:28 -0300

    feat: create professional route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,11 +3,13 @@ import { createAdmin } from "./controllers/admin-controllers/create-admin";
 import { authenticateUser } from "./controllers/user-authenticate-controller";
 import { createCompany } from "./controllers/company-controllers.ts/create-company";
 import { createEmployee } from "./controllers/employee-controllers/create-employee";
+import { createProfessional } from "./controllers/professional-controllers.ts/create-professional";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/admins', createAdmin)
     app.post('/companies', createCompany)
     app.post('/employees', createEmployee)
+    app.post('/professionals', createProfessional)
 
     app.post('/login', authenticateUser)
 }
