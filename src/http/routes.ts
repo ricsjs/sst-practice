1bfd9da02af60b3b099dbdae2f975515e14f61a6
ricsjs <ricfilho0007@gmail.com>
2024-03-11 13:53:51 -0300

    feat: create route to register unit

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,12 +4,14 @@ import { authenticateUser } from "./controllers/user-authenticate-controller";
 import { createCompany } from "./controllers/company-controllers.ts/create-company";
 import { createEmployee } from "./controllers/employee-controllers/create-employee";
 import { createProfessional } from "./controllers/professional-controllers.ts/create-professional";
+import { createUnit } from "./controllers/unit-controllers.ts/create-unit";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/admins', createAdmin)
     app.post('/companies', createCompany)
     app.post('/employees', createEmployee)
     app.post('/professionals', createProfessional)
+    app.post('/units', createUnit)
 
     app.post('/login', authenticateUser)
 }
