6899811fa85160cccfd151eb87a0f239c019f810
ricsjs <ricfilho0007@gmail.com>
2024-03-17 13:29:10 -0300

    fix: correction in the listing, active column check

--- src/repositories/prisma/prisma-employees-repository.ts
+++ src/repositories/prisma/prisma-employees-repository.ts
@@ -3,11 +3,12 @@ import { Empregado, Prisma } from "@prisma/client"
 import { EmployeesRepository } from "../employees-repository"
 
 export class PrismaEmployeesRepository implements EmployeesRepository {
-    
+
     async findMany(companyId: string): Promise<Empregado[]> {
         const employees = await prisma.empregado.findMany({
             where: {
-                companyId: companyId
+                companyId: companyId,
+                active: true
             }
         });
 
