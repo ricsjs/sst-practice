deb3c337c76170084b411f33158a0e5b39f18246
ricsjs <ricfilho0007@gmail.com>
2024-03-21 13:30:53 -0300

    fix: resolving conflicts

--- src/services/factories/employee-factories/make-fetch-employee-by-id.ts
+++ src/services/factories/employee-factories/make-fetch-employee-by-id.ts
@@ -1,5 +1,5 @@
 import { PrismaEmployeesRepository } from "../../../repositories/prisma/prisma-employees-repository"
-import { FetchEmployeeByIdService } from "../../employee-services/fetch-employee-by-id"
+import { FetchEmployeeByIdService } from "../../employee-services/find-employee-by-id"
 
 export function makeFetchEmployeeByIdService() {
     const prismaEmployeesRepository = new PrismaEmployeesRepository
