73da1d5ce4d86fc87733da404497f9d126d1636c
ricsjs <ricfilho0007@gmail.com>
2024-05-02 16:16:38 -0300

    fix: fixes update company

--- src/services/company-services/update-company.ts
+++ src/services/company-services/update-company.ts
@@ -1,9 +1,13 @@
 import { Empresa } from "@prisma/client";
 import { CompaniesRepository } from "../../repositories/companies-repository";
+import { UsersRepository } from "../../repositories/users-repository";
+import { hash } from "bcryptjs";
 
 interface UpdateCompanyServiceRequest {
   id: string;
   userId: string;
+  email: string;
+  password?: string;
   cnpj: string;
   corporate_reason: string;
   fantasy_name: string;
@@ -20,11 +24,14 @@ interface UpdateCompanyServiceResponse {
   company: Empresa;
 }
 export class UpdateCompanyService {
-  constructor(private companiesRepository: CompaniesRepository) {}
+  constructor(
+    private companiesRepository: CompaniesRepository,
+    private usersRepository: UsersRepository
+  ) {}
 
   async execute({
-    id,
-    userId,
+    email,
+    password,
     cnpj,
     corporate_reason,
     fantasy_name,
@@ -37,6 +44,23 @@ export class UpdateCompanyService {
     active,
   }: UpdateCompanyServiceRequest): Promise<UpdateCompanyServiceResponse> {
     try {
+      const user = await this.usersRepository.findByEmail(email);
+
+      if (!user) {
+        throw new Error("User not found!");
+      }
+
+      if (password) {
+        const hashedPassword = await hash(password, 6);
+        await this.usersRepository.update({
+          email,
+          password_hash: hashedPassword,
+          type: "admin"
+        })
+      }
+
+      const oldCompany = await this.companiesRepository.findByUserId(user.id);
+
       const updatedCompany = await this.companiesRepository.update({
         cnpj,
         corporate_reason,
@@ -48,8 +72,8 @@ export class UpdateCompanyService {
         phone,
         dt_start_esocial,
         active,
-        id,
-        userId,
+        id: oldCompany!.id,
+        userId: user.id,
       });
 
       return { company: updatedCompany };
